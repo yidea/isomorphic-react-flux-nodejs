29549433351baa2ad039c65051d5b86b353863a3
ycao2 <ycao@walmartlabs.com>
2015-07-24 16:02:27 -0700

    update wording

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -10,8 +10,8 @@ export default React.createClass({
   render() {
     return (
       <ul>
-        <li><Link to="shelf">Tool: Shelf Suggestion</Link></li>
-        <li><Link to="product-type">Tool: Product Type Suggestion</Link></li>
+        <li><Link to="shelf">Experiment: Shelf Suggestion</Link></li>
+        <li><Link to="product-type">Experiment: Product Type Suggestion</Link></li>
       </ul>
     );
   }
